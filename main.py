45112244ed735c5d40497d628beb67a1c5ade645
ridzp <riddhiman2008@gmail.com>
2021-07-28 10:05:33 +0530

    git checkin  - updated code

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import json
 import socket
+import time
+import paramiko
+
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+flag = 0
 
 with open("C:/Python27/dyn_obj.json") as file:
     data = json.load(file)
-    #print(data["client"]["name"])
-    #data["client"]["name"] = "Riddhiman Phukon"
 
-#    data["dynobj"][0]["dig_new"][0] = "5.5.5.5"
-#    data["dynobj"][0]["dig_new"][1] = "6.6.6.6"
-#    data["dynobj"][0]["dig_new"][2] = "7.7.7.7"
+
 y = range(len(data["dynobj"]))
 for x in y:
     dns_resolve = socket.gethostbyname_ex(data["dynobj"][x]["hostname"])
@@ -17,8 +19,8 @@ for x in y:
     for i in ip_addresses:
         data["dynobj"][x]["dig_new"].append(dns_resolve[2][i])
 
-with open("C:/Python27/dyn_obj.json", 'w') as file:
-    json.dump(data, file, indent = 4)
+#with open("C:/Python27/dyn_obj.json", 'w') as file:
+    #json.dump(data, file, indent = 4)
 
 #-------------------------------completed testing on python ver 2.7
 #--- DNS resolution done and updated dig_new
@@ -43,19 +45,58 @@ for v in u:
                 if(dignew_set != digold_set):
                     ip_add = dignew_set.difference(digold_set)
                     ip_remove = digold_set.difference(dignew_set)
+
+                    if flag == 0:
+                        #ssh_client.get_transport().is_active
+                        ssh_client.connect(hostname= fw_data["firewalls"][v]["fw_ip_address"], port=22, username='admin', password=fw_data["firewalls"][v]["fw_password"],look_for_keys=False, allow_agent=False)
+                        flag = 1
+                        shell = ssh_client.invoke_shell()
+                        time.sleep(2)
+                        shell.send('expert\n')
+                        time.sleep(2)
+#                       shell.send(fw_data["firewalls"][v]["fw_password"]+'\n')
+                        shell.send('qaz123\n')
+                        time.sleep(2)
+                        shell.send('vsenv 1\n')
+                        time.sleep(2)
+
+                        print(ssh_client.get_transport().is_active())
+                        output = shell.recv(10000)
+                        output = output.decode('utf-8')
+                        print(output)
                     if len(ip_add) != 0:
                         print ("Dynmic Object =", data["dynobj"][ex]["object_name"], " firewall = ", fw_data["firewalls"][v]["fw_hostname"])
                         for ax in ip_add:
                             print ("IP to be added" , ax)
+                            shell.send('dynamic_objects -o ' + data["dynobj"][ex]["object_name"] + ' -r ' + ax + ' ' + ax + ' -a\n')
+                            time.sleep(1)
+                            print('dynamic_objects -o ' + data["dynobj"][ex]["object_name"] + ' -r ' + ax + ' ' + ax + ' -a\n')
+                            output = shell.recv(10000)
+                            output = output.decode('utf-8')
+                            print(output)
 
                     if len(ip_remove) != 0:
                         print ("Dynmic Object =", data["dynobj"][ex]["object_name"], " firewall = ", fw_data["firewalls"][v]["fw_hostname"])
                         for rx in ip_remove:
                             print ("IP to be removed" , rx)
+                            shell.send('dynamic_objects -o ' + data["dynobj"][ex]["object_name"] + ' -r ' + rx + ' ' + rx + ' -d\n')
+                            time.sleep(1)
+                            print('dynamic_objects -o ' + data["dynobj"][ex]["object_name"] + ' -r ' + rx + ' ' + rx + ' -d\n')
+                            output = shell.recv(10000)
+                            output = output.decode('utf-8')
+                            print(output)
     object_selector.clear()
+    ssh_client.close()
+    flag = 0
 
+#replaced old dig result IP address with the new dig result
+for bx in y:
+    data["dynobj"][bx]["dig_old"].clear()
+    data["dynobj"][bx]["dig_old"] = data["dynobj"][bx]["dig_new"]
 
-
+#updated dynamic object json file
+with open("C:/Python27/dyn_obj.json", 'w') as file:
+    json.dump(data, file, indent = 4)
 
 
 
